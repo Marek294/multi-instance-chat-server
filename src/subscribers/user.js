29cda8dd58354df2e740ceb0e24fdd13ac3859a0
Marek294 <Markra5835@gmail.com>
2022-03-08 21:57:50 +0100

    prepare dummy subscriber

---
+++ src/subscribers/user.js
@@ -0,0 +1,6 @@
+// const { events, EventEmitter } = require('../utils/EventEmitter')
+// const EmailService = require('../services/email')
+
+// EventEmitter.on(events.user.created, async user => {
+//   await EmailService.sendEmail(user)
+// })
